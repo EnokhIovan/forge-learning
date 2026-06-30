e1459bc3e0dc5ae874a4994d0245c405011b3e1a
EnokhIovan <jacobjovan26@gmail.com>
2026-06-30 23:36:02 +0700

    Create /heal and /get attribut feature

--- src/main/java/com/example/examplemod/command/HelloCommand.java
+++ src/main/java/com/example/examplemod/command/HelloCommand.java
@@ -5,6 +5,9 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import net.minecraft.server.level.ServerPlayer;
+
+import javax.swing.*;
 
 public class HelloCommand {
 
@@ -52,6 +55,48 @@ public class HelloCommand {
         .executes(context -> {
           context.getSource().sendSuccess(
             () -> Component.literal("Halo " + context.getSource().getTextName() + "!\nSelamat datang di mod pertamamu"),
+            false
+          );
+          return 1;
+        })
+    );
+
+    dispatcher.register(
+      Commands.literal("get")
+      .then(
+        Commands.argument("attribut", StringArgumentType.word())
+        .executes(context -> {
+          ServerPlayer player = context.getSource().getPlayerOrException();
+          String attribut = StringArgumentType.getString(context, "attribut");
+          String textContent = switch (attribut){
+            case "health" -> "Darah: " + String.format("%.2f",player.getHealth());
+            default -> {
+              context.getSource().sendFailure(
+                Component.literal("Maaf, attribut tidak dikenali!")
+              );
+              yield null;
+            }
+          };
+
+          if (textContent == null) return 0;
+
+          context.getSource().sendSuccess(
+            () -> Component.literal(textContent),
+            false
+          );
+          return 1;
+        })
+      )
+    );
+
+    dispatcher.register(
+      Commands.literal("heal")
+        .executes(context -> {
+          ServerPlayer player = context.getSource().getPlayerOrException();
+          player.setHealth(Math.min(player.getHealth()*1.2f, player.getMaxHealth()));
+
+          context.getSource().sendSuccess(
+            () -> Component.literal("Halo " + context.getSource().getTextName() + "!\nDarahmu telah bertambah " + String.format("%.2f",player. getHealth() - player.getHealth()/1.2)),
             false
           );
           return 1;
