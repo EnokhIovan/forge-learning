edfaff0a6919f31a0f2e6b2d07c2eba11b1d5d13
EnokhIovan <jacobjovan26@gmail.com>
2026-07-01 16:22:44 +0700

    Adding /feed and /get hunger feature

--- src/main/java/com/example/examplemod/command/HelloCommand.java
+++ src/main/java/com/example/examplemod/command/HelloCommand.java
@@ -6,6 +6,7 @@ import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.server.level.ServerPlayer;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
@@ -38,7 +39,9 @@ public class HelloCommand {
         Commands.argument("attribut", StringArgumentType.word())
         .suggests((context, builder) -> {
           String[] attributes = {
-            "health"
+            "health",
+            "hunger",
+            "level"
           };
 
           for(String attribute : attributes){
@@ -50,7 +53,9 @@ public class HelloCommand {
           ServerPlayer player = context.getSource().getPlayerOrException();
           String attribut = StringArgumentType.getString(context, "attribut");
           String textContent = switch (attribut){
-            case "health" -> "Darah: " + String.format("%.2f",player.getHealth());
+            case "health" -> "HP: " + String.format("%.2f",player.getHealth());
+            case "hunger" -> "Hunger: %d".formatted(player.getFoodData().getFoodLevel());
+            case "level" -> "Level: %d".formatted(player.experienceLevel);
             default -> {
               context.getSource().sendFailure(
                 Component.literal("Maaf, attribut tidak dikenali!")
@@ -102,10 +107,30 @@ public class HelloCommand {
 
             for(ServerPlayer player : players)
               heal(context, player, amount);
+
             return 1;
           })
         )
       )
+    );
+
+    dispatcher.register(
+      Commands.literal("feed")
+        .then(
+          Commands.argument("target", EntityArgument.players())
+            .then(
+              Commands.argument("amount", IntegerArgumentType.integer(1, 20))
+                .executes(context -> {
+                  Collection<ServerPlayer> players = EntityArgument.getPlayers(context, "target");
+                  int amount = IntegerArgumentType.getInteger(context, "amount");
+
+                  for(ServerPlayer player : players)
+                    feed(context, player, amount);
+
+                  return 1;
+                })
+            )
+        )
     );
   }
 
@@ -120,6 +145,23 @@ public class HelloCommand {
       context.getSource().sendSuccess(
         () -> Component.literal(
           player.getName().getString() + " telah dipulihkan sebanyak " + (player.getHealth()-hpBefore) + " HP!"
+        ),
+        false
+      );
+
+      return 1;
+    }
+
+  private static int feed(CommandContext<CommandSourceStack> context, ServerPlayer player, int amount)
+    throws CommandSyntaxException {
+      int hungerBefore = player.getFoodData().getFoodLevel();
+      player.getFoodData().setFoodLevel(
+        Math.min(player.getFoodData().getFoodLevel()+amount, 20)
+      );
+
+      context.getSource().sendSuccess(
+        () -> Component.literal(
+          player.getName().getString() + " telah dipulihkan sebanyak " + (player.getFoodData().getFoodLevel()-hungerBefore) + " hunger!"
         ),
         false
       );
